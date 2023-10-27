f3e912b31e3d42525bde0c277854453d1fd5e3c9
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2023-10-27 10:12:46 +0300

    completed task-2 from homework

--- JavaScript/fe-jshomework-01/script.js
+++ JavaScript/fe-jshomework-01/script.js
@@ -2,3 +2,10 @@
 let name = prompt("What is your name?")
 let response = "Hello,"+" "+ name;
 alert(response);
+
+//Request a user’s year of birth, count his age, and display the result. Store the current year as a constant. 
+let userYear = prompt("What is your birth year?");
+const currentYear = 2023;
+let result = currentYear - userYear;
+alert("You are " + result +" years old");
+
