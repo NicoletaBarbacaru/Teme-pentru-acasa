53ac96fdd0df8b3d8548c4924531cb51769acead
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2023-10-27 16:03:54 +0300

    completed task-06 from homework

--- JavaScript/fe-jshomework-01/script.js
+++ JavaScript/fe-jshomework-01/script.js
@@ -23,5 +23,11 @@ alert("The circle area is " + circleArea);
 //Request a distance in km between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.
 let distance = prompt("What is the distance in km, between Chisinau and Orhei?");
 let timeHours = prompt("What time do you need to cover it?");
-let speed = Math.floor(distance / timeHours);
+let speed = Math.floor(distance / (timeHours));
 alert("You need to drive with "+ speed + " km/h to be on time");
+
+//Create a currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant.
+let USD = prompt("Please provide the amount in USD");
+const exchangeRate = 0.95;
+let amountEuro = USD * exchangeRate;
+alert("You will receive " + amountEuro +" euro for "+ USD + "$")
