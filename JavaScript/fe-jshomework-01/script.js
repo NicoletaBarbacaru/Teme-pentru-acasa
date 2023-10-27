a60399ae2e6529aa1968c8709c2b35db8c2678a8
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2023-10-27 10:21:33 +0300

    completed task-3 from homework

--- JavaScript/fe-jshomework-01/script.js
+++ JavaScript/fe-jshomework-01/script.js
@@ -9,3 +9,7 @@ const currentYear = 2023;
 let result = currentYear - userYear;
 alert("You are " + result +" years old");
 
+//Request a length of a side of a square from a user and display the perimeter of the square.
+let sideLength = prompt("What is the square side length?");
+let perimeter = 4 * sideLength;
+alert("The perimeter of the square is " + perimeter);
