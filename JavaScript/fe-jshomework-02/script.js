bf73f3e887fb1033bac1c3efe03ecfd8edb9741c
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2023-10-31 09:09:26 +0200

    completed task-02 from homework

--- JavaScript/fe-jshomework-02/script.js
+++ JavaScript/fe-jshomework-02/script.js
@@ -9,3 +9,22 @@ if(firstDigit === secondDigit && secondDigit === thirdDigit){
 } else {
     alert("False")
 }
+//Request a five-unit number from a user and check whether it is a palindrome.
+
+let fiveDigitNumber = prompt("Please enter a five-unit number");
+let stringToArray = fiveDigitNumber.toString().split('');
+let reversedArray = fiveDigitNumber.toString().split("").reverse();
+console.log(stringToArray, reversedArray);
+
+for (let i = 0;i < stringToArray.length; i++){
+if(stringToArray[i] === reversedArray[i]){
+    alert("It is a palindrome")
+} else {
+    alert ("It is not a palindrome")
+}}
+
+
+//Request a date (dd:mm:yy) and put out the one that goes after it. Pay attention to the start of a new month, new year, and also leap years.
+
+let date = new Date("14 Oct 2023");
+let currentDate = prompt("Please enter a date", date);
