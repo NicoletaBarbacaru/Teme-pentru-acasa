a60dacce399af987c2e52625ff482198573e64db
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2023-11-06 09:15:06 +0200

    completed task 02 from homework

--- JavaScript/fe-jshomework-03/script.js
+++ JavaScript/fe-jshomework-03/script.js
@@ -1,4 +1,5 @@
-// 1.Write a program that requests a number from a user and finds the summation of every number from 1 to num. For example, for number 4 it is 1 + 2 + 3 + 4 = 10.
+// 1.Write a program that requests a number from a user and finds the summation of every number from 1 to num. 
+//For example, for number 4 it is 1 + 2 + 3 + 4 = 10.
 let number = prompt("Please enter a number");
 
 let sum = 0;
@@ -7,3 +8,23 @@ for( let i = 1; i <= number; i++){
 }
 console.log(sum);
 alert(sum);
+
+// 2.Request two numbers and find only their largest common divisor.
+let number1 = prompt("Please enter number1");
+let number2= prompt("Please enter number2");
+
+let LCD = 0;
+for( let i = 1; i < number1 && i < number2; i++){
+    if(number1 % i == 0 && number2 % i == 0){
+        LCD = i;
+    }
+}
+console.log(LCD);
+
+let n = prompt("Please enter a number");
+let commonDivisor = 0;
+for( let i = 1; i <= n; i++){
+    if( n % i === 0){
+        console.log(i)
+    }
+}
